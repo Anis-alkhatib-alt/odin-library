0b4ee9a7bbc2bbcead3ab76092f64599e8866be4
anis alkhatib <alkhatib.anis@proton.me>
2024-10-16 23:09:03 +0300

    final

--- index.js
+++ index.js
@@ -7,40 +7,68 @@ const book = function(title, author, pages, read) {
   this.read = read;
 }
 
+book.prototype.toggleRead = function() {
+  this.read = !this.read;
+}
+
+const removeBook = function (index) {
+  library.splice(index, 1);
+}
+
 const addBookToLibrary = function() {
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const pages = document.querySelector("#pages").value;
-  const read = document.querySelector("#read").value;
+  const read = document.querySelector("#read").checked;
   const newBook = new book(title, author, pages, read);
   library.push(newBook);
   render();
   console.log(library)
 }
 
-const render = function () {
+const render = function() {
   const display = document.querySelector(".display");
   display.innerHTML = ""
   for (let i = 0; i < library.length; i++) {
+    const addedBook = library[i];
     const bookTile = document.createElement("div");
     bookTile.className = "book-tile";
     display.appendChild(bookTile);
-
+    
     const bookTitle = document.createElement("p");
-    bookTitle.innerText = `Book Title: ${library[i].title}`;
+    bookTitle.innerText = `Book Title: ${addedBook.title}`;
     bookTile.appendChild(bookTitle);
 
     const bookAuthor = document.createElement("p");
-    bookAuthor.innerText = `Book Author: ${library[i].author}`;
+    bookAuthor.innerText = `Book Author: ${addedBook.author}`;
     bookTile.appendChild(bookAuthor);
 
     const bookPages = document.createElement("p");
-    bookPages.innerText = `Book Pages: ${library[i].pages}`;
+    bookPages.innerText = `Book Pages: ${addedBook.pages}`;
     bookTile.appendChild(bookPages);
 
     const bookRead = document.createElement("p");
-    bookRead.innerText = `Book Read: ${library[i].read}`;
-    bookTile.appendChild(bookRead);
+    bookRead.innerText = `Book Read: ${addedBook.read ? "Yes" : "No"}`
+    bookTile.appendChild(bookRead)
+
+    const toggleReadCheckbox = document.createElement("input");
+    toggleReadCheckbox.type = "checkbox";
+    toggleReadCheckbox.checked = addedBook.read;
+    bookTile.appendChild(toggleReadCheckbox);
+
+    toggleReadCheckbox.addEventListener("change", function() {
+      addedBook.toggleRead();
+      render();
+    })
+
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove Book"
+    bookTile.appendChild(removeButton);
+
+    removeButton.addEventListener("click", function() {
+      removeBook(i);
+      render();
+    })
   }
 }
 
@@ -54,6 +82,6 @@ const bookForm = document.querySelector(".book-form");
 bookForm.addEventListener("submit", function(e) {
   e.preventDefault();
   addBookToLibrary();
-  bookForm.reset()
-  bookDialog.close()
+  bookForm.reset();
+  bookDialog.close();
 })
